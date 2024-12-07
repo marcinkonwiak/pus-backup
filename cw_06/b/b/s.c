1cf7b0865f6228d7577a151573e0329dd6ad05d7
Marcin Konwiak <marcin2899@gmail.com>
2024-12-07 22:18:51 +0100

    6bb

--- cw_06/b/b/s.c
+++ cw_06/b/b/s.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <syslog.h>
 
 #define PORT 2341
 #define BUFFER_SIZE 1024
@@ -12,9 +13,11 @@ int sockfd;
 
 void handle_exit(int sig) {
     printf("\nClosing server\n");
+    syslog(LOG_INFO, "UDP Server shutting down.");
     if (sockfd >= 0) {
         close(sockfd);
     }
+    closelog();
     exit(0);
 }
 
@@ -23,15 +26,16 @@ int main() {
     socklen_t addr_len;
     char buffer[BUFFER_SIZE];
 
+    openlog("udp_server", LOG_PID | LOG_CONS, LOG_USER);
+
     signal(SIGINT, handle_exit);
 
-    // Create UDP socket
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("socket failed");
+        syslog(LOG_ERR, "Socket creation failed.");
         exit(EXIT_FAILURE);
     }
 
-    // Bind to the specified port
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family      = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -39,11 +43,14 @@ int main() {
 
     if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
+        syslog(LOG_ERR, "Binding failed on port %d.", PORT);
         close(sockfd);
+        closelog();
         exit(EXIT_FAILURE);
     }
 
     printf("UDP Server listening for broadcasts on port %d...\n", PORT);
+    syslog(LOG_INFO, "UDP Server started on port %d.", PORT);
 
     while (1) {
         memset(buffer, 0, sizeof(buffer));
@@ -52,23 +59,26 @@ int main() {
         int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_len);
         if (n < 0) {
             perror("recvfrom error");
+            syslog(LOG_ERR, "recvfrom error occurred.");
             continue;
         }
 
         buffer[n] = '\0';
-        printf("Received broadcast message: '%s' from %s:%d\n",
-            buffer,
-            inet_ntoa(client_addr.sin_addr),
-            ntohs(client_addr.sin_port)
-        );
+        char *client_ip = inet_ntoa(client_addr.sin_addr);
+        int client_port = ntohs(client_addr.sin_port);
+
+        printf("Received broadcast message: '%s' from %s:%d\n", buffer, client_ip, client_port);
+        syslog(LOG_INFO, "Received message '%s' from %s:%d", buffer, client_ip, client_port);
 
         // Respond to client
         const char *response = "2";
         if (sendto(sockfd, response, strlen(response), 0, (struct sockaddr*)&client_addr, addr_len) < 0) {
             perror("sendto error");
+            syslog(LOG_ERR, "sendto error occurred while responding to %s:%d", client_ip, client_port);
         }
     }
 
     close(sockfd);
+    closelog();
     return 0;
 }
