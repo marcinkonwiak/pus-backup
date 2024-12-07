fbe1e839cca37cf27a560eaf0facddc12e819ac0
Marcin Konwiak <marcin2899@gmail.com>
2024-12-07 22:05:08 +0100

    6ba

--- cw_06/b/a/s.c
+++ cw_06/b/a/s.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <syslog.h>
+#include <netdb.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -15,14 +17,16 @@ float calculate(const float num1, const char operator, const float num2) {
         case '+': return num1 + num2;
         case '-': return num1 - num2;
         case '*': return num1 * num2;
-        case '/': return (num2 != 0) ? num1 / num2 : 0;  // Avoid division by zero
-        default: return 0; // Invalid operator
+        case '/': return (num2 != 0) ? num1 / num2 : 0;
+        default: return 0;
     }
 }
 
 void handle_sigint(int sig) {
     printf("\nClosing server\n");
+    syslog(LOG_INFO, "UDP server shutting down.");
     close(server_fd);
+    closelog();
     exit(0);
 }
 
@@ -31,17 +35,22 @@ int main() {
     socklen_t clilen;
     char buffer[BUFFER_SIZE] = {0};
 
+    openlog("udp_server", LOG_PID | LOG_CONS, LOG_USER);
+
     signal(SIGINT, handle_sigint);
 
     if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
+        syslog(LOG_ERR, "Socket creation failed.");
         exit(EXIT_FAILURE);
     }
 
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         perror("setsockopt failed");
+        syslog(LOG_ERR, "setsockopt failed.");
         close(server_fd);
+        closelog();
         exit(EXIT_FAILURE);
     }
 
@@ -52,11 +61,14 @@ int main() {
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
+        syslog(LOG_ERR, "Bind failed.");
         close(server_fd);
+        closelog();
         exit(EXIT_FAILURE);
     }
 
     printf("UDP Server running on port %d...\n", PORT);
+    syslog(LOG_INFO, "UDP server started on port %d.", PORT);
 
     while (1) {
         memset(buffer, 0, BUFFER_SIZE);
@@ -64,8 +76,13 @@ int main() {
         int n = recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&cliaddr, &clilen);
         if (n < 0) {
             perror("recvfrom failed");
+            syslog(LOG_ERR, "recvfrom failed.");
             continue;
         }
+
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cliaddr.sin_addr, client_ip, INET_ADDRSTRLEN);
+        syslog(LOG_INFO, "Received request from %s:%d", client_ip, ntohs(cliaddr.sin_port));
 
         float num1, num2;
         char operator;
@@ -82,5 +99,6 @@ int main() {
     }
 
     close(server_fd);
+    closelog();
     return 0;
 }
