9c2d5930e1a5dd1e385e75c10f78df760c7bf3d6
Marcin Konwiak <marcin2899@gmail.com>
2024-12-07 19:56:54 +0100

    5

---
+++ cw_04_cw/k.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/select.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int sock = 0;
+
+void handle_exit(int sig) {
+    printf("\nClosing\n");
+    struct linger so_linger;
+    so_linger.l_onoff = 1;
+    so_linger.l_linger = 0;
+
+    if (setsockopt(sock, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger)) < 0) {
+        perror("setsockopt failed");
+    }
+
+    close(sock);
+    exit(0);
+}
+
+int main() {
+    struct sockaddr_in server_address;
+    char buffer[BUFFER_SIZE] = {0};
+    char message[100];
+    fd_set readfds;
+    int maxfd;
+    int prompt_printed = 0;
+    int stdineof = 0;
+
+    signal(SIGINT, handle_exit);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\nSocket creation error\n");
+        return -1;
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+        printf("\nInvalid address\n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        printf("\nConnection Failed\n");
+        return -1;
+    }
+
+    while (1) {
+        if (!prompt_printed) {
+            printf("Enter expression (e.g. 1 + 1): ");
+            fflush(stdout);
+            prompt_printed = 1;
+        }
+
+        FD_ZERO(&readfds);
+        if (stdineof == 0) {
+            FD_SET(0, &readfds);
+        }
+        FD_SET(sock, &readfds);
+
+        maxfd = sock > 0 ? sock : 0;
+
+        int activity = select(maxfd + 1, &readfds, NULL, NULL, NULL);
+
+        if (activity < 0 && errno != EINTR) {
+            printf("Select error\n");
+            break;
+        }
+
+        if (FD_ISSET(0, &readfds)) {
+            char input[256];
+            if (fgets(input, sizeof(input), stdin) != NULL) {
+                prompt_printed = 0;
+                size_t len = strlen(input);
+                if (len > 0 && input[len - 1] == '\n') {
+                    input[len - 1] = '\0';
+                }
+
+                float num1, num2;
+                char operator;
+                int i = sscanf(input, "%f %c %f", &num1, &operator, &num2);
+                if (i == 3) {
+                    snprintf(message, sizeof(message), "%f %c %f", num1, operator, num2);
+                    send(sock, message, strlen(message), 0);
+                } else if (i == -1) {
+                    stdineof = 1;
+                    shutdown(sock, SHUT_WR);
+                    FD_CLR(0, &readfds);
+                    continue;
+                }
+                else {
+                    printf("Invalid input\n");
+                    continue;
+                }
+            }
+        }
+
+        if (FD_ISSET(sock, &readfds)) {
+            memset(buffer, 0, sizeof(buffer));
+            int valread = read(sock, buffer, BUFFER_SIZE);
+            if (valread > 0) {
+                printf("\033[2K\n");
+                printf("Result: %s\n", buffer);
+                prompt_printed = 0;
+            } else if (valread == 0) {
+                printf("Server closed connection\n");
+                break;
+            } else {
+                perror("Read error");
+                break;
+            }
+        }
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ cw_05/s.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int server_fd;
+
+float calculate(const float num1, const char operator, const float num2) {
+    switch (operator) {
+        case '+': return num1 + num2;
+        case '-': return num1 - num2;
+        case '*': return num1 * num2;
+        case '/': return (num2 != 0) ? num1 / num2 : 0;  // Avoid division by zero
+        default: return 0; // Invalid operator
+    }
+}
+
+void handle_sigint(int sig) {
+    printf("\nClosing server\n");
+    close(server_fd);
+    exit(0);
+}
+
+int main() {
+    struct sockaddr_in address, cliaddr;
+    socklen_t clilen;
+    char buffer[BUFFER_SIZE] = {0};
+
+    signal(SIGINT, handle_sigint);
+
+    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("UDP Server running on port %d...\n", PORT);
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        clilen = sizeof(cliaddr);
+        int n = recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&cliaddr, &clilen);
+        if (n < 0) {
+            perror("recvfrom failed");
+            continue;
+        }
+
+        float num1, num2;
+        char operator;
+        if (sscanf(buffer, "%f %c %f", &num1, &operator, &num2) == 3) {
+            float result = calculate(num1, operator, num2);
+            snprintf(buffer, BUFFER_SIZE, "%f", result);
+            sendto(server_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&cliaddr, clilen);
+            printf("%f %c %f = %f\n", num1, operator, num2, result);
+        } else {
+            // Invalid input format, send an error response
+            char *err_msg = "Invalid input.";
+            sendto(server_fd, err_msg, strlen(err_msg), 0, (struct sockaddr *)&cliaddr, clilen);
+        }
+    }
+
+    close(server_fd);
+    return 0;
+}
