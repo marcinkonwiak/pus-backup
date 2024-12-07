d1a90fa40b221d2eebae3de602e353e6f5371a1d
Marcin Konwiak <marcin2899@gmail.com>
2024-12-07 21:59:57 +0100

    6a

--- cw_06/a/s.c
+++ cw_06/a/s.c
@@ -5,6 +5,9 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <syslog.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -44,7 +47,9 @@ void handle_client(int conn_fd) {
 
 void handle_sigint(int sig) {
     printf("\nClosing server\n");
+    syslog(LOG_INFO, "Server shutting down.");
     close(server_fd);
+    closelog();
     exit(0);
 }
 
@@ -52,6 +57,7 @@ void sig_chld(int signo) {
     int stat;
     while (waitpid(-1, &stat, WNOHANG) > 0) {
         printf("Client disconnected\n");
+        syslog(LOG_INFO, "Client disconnected.");
     }
 }
 
@@ -65,32 +71,45 @@ int main() {
     char buffer[BUFFER_SIZE] = {0};
     int conn_fd;
 
+    openlog("my_server", LOG_PID | LOG_CONS, LOG_USER);
+
     signal(SIGINT, handle_sigint);
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
+        syslog(LOG_ERR, "Socket creation failed.");
         exit(EXIT_FAILURE);
     }
 
     if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("Bind failed");
+        syslog(LOG_ERR, "Binding failed.");
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, 3) < 0) {
         perror("Listen failed");
+        syslog(LOG_ERR, "Listening failed.");
         exit(EXIT_FAILURE);
     }
 
     signal(SIGCHLD, sig_chld);
 
     printf("Server running on port %d...\n", PORT);
+    syslog(LOG_INFO, "Server started on port %d.", PORT);
 
     while (1) {
         if ((conn_fd = accept(server_fd, (struct sockaddr *) &address,
                               (socklen_t *) &addrlen)) < 0) {
             perror("Accept failed");
+            syslog(LOG_ERR, "Accept failed.");
             exit(EXIT_FAILURE);
         }
+
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &address.sin_addr, client_ip, INET_ADDRSTRLEN);
+        syslog(LOG_INFO, "Client connected from %s:%d", client_ip, ntohs(address.sin_port));
+        printf("Client connected from %s:%d\n", client_ip, ntohs(address.sin_port));
 
         if (fork() == 0) {
             close(server_fd);
@@ -101,4 +120,7 @@ int main() {
 
         close(conn_fd);
     }
+
+    closelog();
+    return 0;
 }
