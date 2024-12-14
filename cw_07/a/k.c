15e94e467dadab6f8f4181b0aee7dad38b994fc5
Marcin Konwiak <marcin2899@gmail.com>
2024-12-14 20:36:16 +0100

    7a

--- cw_07/a/k.c
+++ cw_07/a/k.c
@@ -5,13 +5,25 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <errno.h>
-#include <sys/select.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <sys/socket.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
 int sock = 0;
-struct sockaddr_in server_address;
+
+// Compute checksum for IP/UDP headers
+unsigned short csum(unsigned short *buf, int nwords) {
+    unsigned long sum = 0;
+    for (; nwords > 0; nwords--) {
+        sum += *buf++;
+    }
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    return (unsigned short)(~sum);
+}
 
 void handle_exit(int sig) {
     printf("\nClosing\n");
@@ -20,110 +32,90 @@ void handle_exit(int sig) {
 }
 
 int main() {
-    char buffer[BUFFER_SIZE] = {0};
+    char buffer[BUFFER_SIZE];
     char message[100];
-    fd_set readfds;
-    int maxfd;
-    int prompt_printed = 0;
-    int stdineof = 0;
-
-    struct timeval tv;
     signal(SIGINT, handle_exit);
 
-    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        printf("\nSocket creation error\n");
+    // Create raw socket
+    if ((sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) {
+        perror("Socket creation error");
         return -1;
     }
 
+    // Tell the OS we're building the IP header ourselves
+    int one = 1;
+    if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
+        perror("setsockopt error");
+        close(sock);
+        return -1;
+    }
+
+    struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
 
     if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
-        printf("\nInvalid address\n");
+        printf("\nInvalid server address\n");
         return -1;
     }
 
-
     while (1) {
-    if (!prompt_printed) {
         printf("Enter expression (e.g. 1 + 1): ");
-        fflush(stdout);
-        prompt_printed = 1;
-    }
-
-    FD_ZERO(&readfds);
-    if (stdineof == 0) {
-        FD_SET(STDIN_FILENO, &readfds);
-    }
-    FD_SET(sock, &readfds);
-
-    maxfd = sock > STDIN_FILENO ? sock : STDIN_FILENO;
-
-    struct timeval timeout;
-    timeout.tv_sec = 5; 
-    timeout.tv_usec = 0;
-
-    int activity = select(maxfd + 1, &readfds, NULL, NULL, &timeout);
-
-    if (activity < 0 && errno != EINTR) {
-        printf("Select error\n");
-        break;
-    }
-
-    if (activity == 0) {
-        printf("\nServer closed\n");
-        prompt_printed = 0;
-        exit(0);
-    }
-
-    if (FD_ISSET(STDIN_FILENO, &readfds)) {
-        char input[256];
-        if (fgets(input, sizeof(input), stdin) != NULL) {
-            prompt_printed = 0;
-            size_t len = strlen(input);
-            if (len > 0 && input[len - 1] == '\n') {
-                input[len - 1] = '\0';
+        if (fgets(message, sizeof(message), stdin) != NULL) {
+            size_t len = strlen(message);
+            if (len > 0 && message[len - 1] == '\n') {
+                message[len - 1] = '\0';
             }
 
-            float num1, num2;
-            char operator;
-            int i = sscanf(input, "%f %c %f", &num1, &operator, &num2);
-            if (i == 3) {
-                snprintf(message, sizeof(message), "%f %c %f", num1, operator, num2);
-                if (sendto(sock, message, strlen(message), 0, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-                    perror("Error sending data to server");
-                    continue;
-                }
-            } else if (i == -1) {
-                break;
-            } else {
-                printf("Invalid input\n");
+            // Buffer to build the packet
+            char packet[BUFFER_SIZE];
+            memset(packet, 0, BUFFER_SIZE);
+
+            // IP header
+            struct ip *ip_header = (struct ip *)packet;
+            ip_header->ip_hl = 5;  // Header length
+            ip_header->ip_v = 4;   // IPv4
+            ip_header->ip_tos = 0;
+            ip_header->ip_len = htons(sizeof(struct ip) + sizeof(struct udphdr) + strlen(message));
+            ip_header->ip_id = htonl(54321);
+            ip_header->ip_off = 0;
+            ip_header->ip_ttl = 64;
+            ip_header->ip_p = IPPROTO_UDP;
+
+            // Spoofed source IP
+            if (inet_pton(AF_INET, "192.168.1.11", &ip_header->ip_src) <= 0) {
+                printf("\nInvalid fake IP address\n");
                 continue;
             }
-        } else {
-            stdineof = 1;
-        }
-    }
 
-    if (FD_ISSET(sock, &readfds)) {
-        memset(buffer, 0, sizeof(buffer));
-        struct sockaddr_in from;
-        socklen_t fromlen = sizeof(from);
-        int valread = recvfrom(sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&from, &fromlen);
-        if (valread > 0) {
-            printf("\033[2K\n");
-            printf("Result: %s\n", buffer);
-            prompt_printed = 0;
-        } else if (valread == 0) {
-            printf("Server closed connection or no data\n");
-            break;
-        } else {
-            perror("Error receiving data from server");
-            break;
+            // Destination IP
+            ip_header->ip_dst = server_address.sin_addr;
+
+            // Calculate IP checksum
+            ip_header->ip_sum = 0;
+            ip_header->ip_sum = csum((unsigned short *)packet, sizeof(struct ip) / 2);
+
+            // UDP header
+            struct udphdr *udp_header = (struct udphdr *)(packet + sizeof(struct ip));
+            udp_header->uh_sport = htons(54321);  // Fake source port
+            udp_header->uh_dport = htons(PORT);  // Destination port
+            udp_header->uh_ulen = htons(sizeof(struct udphdr) + strlen(message));
+            udp_header->uh_sum = 0;  // UDP checksum (optional, can be 0)
+
+            // Copy the message into the packet
+            strcpy(packet + sizeof(struct ip) + sizeof(struct udphdr), message);
+
+            // Send the packet
+            if (sendto(sock, packet, sizeof(struct ip) + sizeof(struct udphdr) + strlen(message), 0,
+                       (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+                perror("Error sending packet");
+                continue;
+            }
+
+            printf("Sent spoofed packet: '%s'\n", message);
         }
     }
-}
 
     close(sock);
     return 0;
