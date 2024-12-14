a4232b47c6de841ec589453b673c461e6258706a
Marcin Konwiak <marcin2899@gmail.com>
2024-12-14 21:43:20 +0100

    7b ---

--- cw_07/b/k.c
+++ cw_07/b/k.c
@@ -3,125 +3,112 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <signal.h>
-#include <errno.h>
-#include <sys/select.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
-
-int sock = 0;
-
-void handle_exit(int sig) {
-    printf("\nClosing\n");
-    struct linger so_linger;
-    so_linger.l_onoff = 1;
-    so_linger.l_linger = 0;
-
-    if (setsockopt(sock, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger)) < 0) {
-        perror("setsockopt failed");
-    }
-
-    close(sock);
-    exit(0);
+// Compute checksum for IP and TCP headers
+unsigned short csum(unsigned short *buf, int nwords) {
+    unsigned long sum = 0;
+    for (; nwords > 0; nwords--)
+        sum += *buf++;
+    sum = (sum >> 16) + (sum &0xffff);
+    sum += (sum >> 16);
+    return (unsigned short)(~sum);
 }
 
 int main() {
-    struct sockaddr_in server_address;
-    char buffer[BUFFER_SIZE] = {0};
-    char message[100];
-    fd_set readfds;
-    int maxfd;
-    int prompt_printed = 0;
-    int stdineof = 0;
-
-    signal(SIGINT, handle_exit);
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\nSocket creation error\n");
-        return -1;
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
     }
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-
-    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
-        printf("\nInvalid address\n");
-        return -1;
+    // We provide our own IP header
+    int one = 1;
+    if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
     }
 
-    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        printf("\nConnection Failed\n");
-        return -1;
+    // Fake source IP and real destination IP
+    const char *source_ip = "1.2.3.4";    // FAKE IP
+    const char *dest_ip   = "127.0.0.1";  // Server IP (localhost example)
+    unsigned short source_port = 12345;   // Arbitrary
+    unsigned short dest_port = 8080;      // Server port
+
+    char packet[4096];
+    memset(packet, 0, sizeof(packet));
+
+    struct iphdr *ip = (struct iphdr *)packet;
+    struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct iphdr));
+    char *data = (char *)(packet + sizeof(struct iphdr) + sizeof(struct tcphdr));
+    strcpy(data, "1 + 1"); // The payload (not a valid TCP handshake, just demonstration)
+
+    // Fill IP header
+    ip->ihl = 5;
+    ip->version = 4;
+    ip->tos = 0;
+    ip->tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr) + strlen(data));
+    ip->id = htons(54321);
+    ip->frag_off = 0;
+    ip->ttl = 64;
+    ip->protocol = IPPROTO_TCP;
+    ip->saddr = inet_addr(source_ip);
+    ip->daddr = inet_addr(dest_ip);
+    ip->check = 0; // Will calculate later
+
+    // Fill TCP header
+    tcp->source = htons(source_port);
+    tcp->dest = htons(dest_port);
+    tcp->seq = htonl(0);
+    tcp->ack_seq = 0;
+    tcp->doff = 5; // data offset
+    tcp->syn = 1;  // Pretend this is a SYN packet
+    tcp->window = htons(65535);
+    tcp->check = 0;
+    tcp->urg_ptr = 0;
+
+    // Pseudo-header for checksum calculation
+    struct pseudo_header {
+        unsigned int src_addr;
+        unsigned int dst_addr;
+        unsigned char placeholder;
+        unsigned char protocol;
+        unsigned short tcp_length;
+    } psh;
+
+    psh.src_addr = inet_addr(source_ip);
+    psh.dst_addr = inet_addr(dest_ip);
+    psh.placeholder = 0;
+    psh.protocol = IPPROTO_TCP;
+    psh.tcp_length = htons(sizeof(struct tcphdr) + strlen(data));
+
+    int psize = sizeof(struct pseudo_header) + sizeof(struct tcphdr) + strlen(data);
+    char *pseudogram = malloc(psize);
+    memcpy(pseudogram, (char *)&psh, sizeof(struct pseudo_header));
+    memcpy(pseudogram + sizeof(struct pseudo_header), tcp, sizeof(struct tcphdr) + strlen(data));
+
+    tcp->check = csum((unsigned short *)pseudogram, psize/2);
+
+    // IP checksum
+    ip->check = csum((unsigned short *)packet, (sizeof(struct iphdr))/2);
+
+    // Destination
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = tcp->dest;
+    sin.sin_addr.s_addr = ip->daddr;
+
+    // Send packet
+    if (sendto(sockfd, packet, ntohs(ip->tot_len), 0, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+        perror("sendto");
+        exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        if (!prompt_printed) {
-            printf("Enter expression (e.g. 1 + 1): ");
-            fflush(stdout);
-            prompt_printed = 1;
-        }
+    printf("Spoofed packet sent.\n");
 
-        FD_ZERO(&readfds);
-        if (stdineof == 0) {
-            FD_SET(0, &readfds);
-        }
-        FD_SET(sock, &readfds);
-
-        maxfd = sock > 0 ? sock : 0;
-
-        int activity = select(maxfd + 1, &readfds, NULL, NULL, NULL);
-
-        if (activity < 0 && errno != EINTR) {
-            printf("Select error\n");
-            break;
-        }
-
-        if (FD_ISSET(0, &readfds)) {
-            char input[256];
-            if (fgets(input, sizeof(input), stdin) != NULL) {
-                prompt_printed = 0;
-                size_t len = strlen(input);
-                if (len > 0 && input[len - 1] == '\n') {
-                    input[len - 1] = '\0';
-                }
-
-                float num1, num2;
-                char operator;
-                int i = sscanf(input, "%f %c %f", &num1, &operator, &num2);
-                if (i == 3) {
-                    snprintf(message, sizeof(message), "%f %c %f", num1, operator, num2);
-                    send(sock, message, strlen(message), 0);
-                } else if (i == -1) {
-                    stdineof = 1;
-                    shutdown(sock, SHUT_WR);
-                    FD_CLR(0, &readfds);
-                    continue;
-                }
-                else {
-                    printf("Invalid input\n");
-                    continue;
-                }
-            }
-        }
-
-        if (FD_ISSET(sock, &readfds)) {
-            memset(buffer, 0, sizeof(buffer));
-            int valread = read(sock, buffer, BUFFER_SIZE);
-            if (valread > 0) {
-                printf("\033[2K\n");
-                printf("Result: %s\n", buffer);
-                prompt_printed = 0;
-            } else if (valread == 0) {
-                printf("Server closed connection\n");
-                break;
-            } else {
-                perror("Read error");
-                break;
-            }
-        }
-    }
-
-    close(sock);
+    free(pseudogram);
+    close(sockfd);
     return 0;
 }
--- cw_07/b/s.c
+++ cw_07/b/s.c
@@ -2,169 +2,85 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <arpa/inet.h>
-#include <signal.h>
-#include <sys/wait.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
-#define MAX_IPS 100
+int main() {
+    int sockfd;
+    unsigned char buffer[65535];
+    struct sockaddr_in src_addr;
+    socklen_t addrlen = sizeof(src_addr);
+    int server_port = 8080; // change to your desired port
 
-int server_fd;
-char *allowed_ips[MAX_IPS + 1]; // List of allowed IPs
-int allowed_ip_count = 0;
-
-int load_allowed_ips(const char *filename) {
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
-        perror("fopen");
-        return -1;
+    // Create a raw socket that receives TCP packets (IP level)
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
     }
 
-    char line[256];
-    while (fgets(line, sizeof(line), fp) && allowed_ip_count < MAX_IPS) {
-        line[strcspn(line, "\n")] = '\0';
-        if (line[0] == '\0') {
-            continue;
-        }
+    printf("Server running");
 
-        allowed_ips[allowed_ip_count] = strdup(line);
-        if (!allowed_ips[allowed_ip_count]) {
-            perror("strdup");
-            fclose(fp);
-            return -1;
-        }
-        allowed_ip_count++;
-    }
-
-    allowed_ips[allowed_ip_count] = NULL;
-    fclose(fp);
-    return 0;
-}
-
-int is_ip_allowed(const char *ip) {
-    for (int i = 0; i < allowed_ip_count; i++) {
-        if (strcmp(ip, allowed_ips[i]) == 0) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-float calculate(const float num1, const char operator, const float num2) {
-    switch (operator) {
-        case '+': return num1 + num2;
-        case '-': return num1 - num2;
-        case '*': return num1 * num2;
-        case '/': return (num2 != 0) ? num1 / num2 : 0;
-        default: return 0;
-    }
-}
-
-void handle_client(int conn_fd) {
-    char buffer[BUFFER_SIZE] = {0};
+    // Receive packets in a loop
     while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        int valread = read(conn_fd, buffer, BUFFER_SIZE);
-        if (valread <= 0) {
+        ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+                             (struct sockaddr *)&src_addr, &addrlen);
+        if (n < 0) {
+            if (errno == EINTR)
+                continue;
+            perror("recvfrom");
             break;
         }
 
-        float num1, num2;
-        char operator;
-        sscanf(buffer, "%f %c %f", &num1, &operator, &num2);
-        float result = calculate(num1, operator, num2);
-
-        snprintf(buffer, BUFFER_SIZE, "%f", result);
-        send(conn_fd, buffer, strlen(buffer), 0);
-
-        printf("%f %c %f = %f\n", num1, operator, num2, result);
-    }
-}
-
-void handle_sigint(int sig) {
-    printf("\nClosing server\n");
-    close(server_fd);
-
-    for (int i = 0; i < allowed_ip_count; i++) {
-        free(allowed_ips[i]);
-    }
-
-    exit(0);
-}
-
-void sig_chld(int signo) {
-    int stat;
-    while (waitpid(-1, &stat, WNOHANG) > 0) {
-        printf("Client disconnected\n");
-    }
-}
-
-int main() {
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    char buffer[BUFFER_SIZE] = {0};
-    int conn_fd;
-
-    if (load_allowed_ips("ip.txt") < 0) {
-        fprintf(stderr, "Failed to load allowed IP addresses\n");
-        exit(EXIT_FAILURE);
-    }
-
-    signal(SIGINT, handle_sigint);
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
-    }
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-
-    signal(SIGCHLD, sig_chld);
-
-    printf("Server running on port %d...\n", PORT);
-
-    while (1) {
-        if ((conn_fd = accept(server_fd, (struct sockaddr *) &address,
-                              (socklen_t *) &addrlen)) < 0) {
-            perror("Accept failed");
-            exit(EXIT_FAILURE);
-        }
-
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &address.sin_addr, client_ip, sizeof(client_ip));
-
-        if (!is_ip_allowed(client_ip)) {
-            printf("Connection from unauthorized IP %s rejected\n", client_ip);
-            close(conn_fd);
+        if (n < (int)sizeof(struct iphdr)) {
+            // Packet too short to contain an IP header
             continue;
         }
 
-        printf("Connection accepted from %s\n", client_ip);
-
-        if (fork() == 0) {
-            close(server_fd);
-            handle_client(conn_fd);
-            close(conn_fd);
-            exit(0);
+        struct iphdr *ip = (struct iphdr *)buffer;
+        int ip_header_len = ip->ihl * 4;
+        if (ip_header_len < 20 || ip_header_len > n) {
+            continue;
         }
 
-        close(conn_fd);
+        if (ip->protocol != IPPROTO_TCP) {
+            continue;
+        }
+
+        if (n < ip_header_len + (int)sizeof(struct tcphdr)) {
+            continue;
+        }
+
+        struct tcphdr *tcp = (struct tcphdr *)(buffer + ip_header_len);
+        int tcp_header_len = tcp->doff * 4;
+        if (tcp_header_len < 20 || ip_header_len + tcp_header_len > n) {
+            continue;
+        }
+
+        if (ntohs(tcp->dest) != server_port) {
+            continue;
+        }
+
+        // Calculate payload length
+        int payload_len = n - (ip_header_len + tcp_header_len);
+        unsigned char *payload = (unsigned char *)(buffer + ip_header_len + tcp_header_len);
+
+        // Print source info and payload
+        struct in_addr saddr;
+        saddr.s_addr = ip->saddr;
+
+        printf("Received packet from %s:%d\n", inet_ntoa(saddr), ntohs(tcp->source));
+
+        if (payload_len > 0) {
+            printf("received data: %.*s\n", payload_len, payload);
+        } else {
+            printf("No payload.\n");
+        }
     }
+
+    close(sockfd);
+    return 0;
 }
